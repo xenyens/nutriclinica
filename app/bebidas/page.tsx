0844e8c055cbee982ea7d31512aef99aeb88938d
xenyens <xpadilla@ensenada.tecnm.mx>
2023-05-09 18:49:35 -0700

    version 1.9.2

--- app/bebidas/page.tsx
+++ app/bebidas/page.tsx
@@ -3,6 +3,10 @@ import { CardBeverage } from '@/components/components/CardBeverage';
 
 import bebida01 from '../../public/jugoverde.jpg'
 import bebida02 from '../../public/jugopepino.jpg'
+import bebida03 from '../../public/smoothiefrutosrojos.jpg'
+import bebida04 from '../../public/batidoplatanoavena.jpg'
+import bebida05 from '../../public/infusiongengibrelimon.jpg'
+import bebida06 from '../../public/teverdeconmenta.jpg'
 
 
 const ingredientes = [
@@ -29,6 +33,34 @@ export default function Bebidas() {
             instructions={['Coloca el pepino, el jugo de limón y las hojas de menta en una jarra.','Machaca ligeramente los ingredientes con un mortero para liberar los sabores.','Agrega el agua y revuelve bien.','Añade hielo al gusto y deja reposar en el refrigerador durante al menos 1 hora para que los sabores se mezclen.','Sirve frío y disfruta de esta refrescante agua de pepino y limón']}
         />
         
+        <CardBeverage
+            imagen={bebida03}
+            title='Smoothie de frutos rojos'
+            ingredients={['1 taza de frutos rojos (fresas, frambuesas, moras, arándanos, etc.)','1 plátano maduro','1 taza de leche de almendras (o cualquier otra leche vegetal)','1 cucharada de miel o sirope vegetal (opcional)','Hielo al gusto']}
+            instructions={['Coloca todos los ingredientes en una licuadora.','Licua hasta obtener una mezcla suave y cremosa.','Agrega hielo si lo deseas y vuelve a licuar por unos segundos.','Sirve en un vaso y disfruta.']}
+        />
+
+        <CardBeverage
+            imagen={bebida04}
+            title='Batido de plátano y avena'
+            ingredients={['1 plátano maduro','1 taza de leche de almendras (o cualquier leche vegetal)','1/4 de taza de avena en hojuelas','1 cucharada de miel o sirope de arce (opcional)','Canela en polvo al gusto','Hielo al gusto']}
+            instructions={['En una licuadora, agrega el plátano, la leche de almendras, la avena, la miel y la canela.','Licua hasta obtener una mezcla suave y cremosa.','Agrega hielo si así lo deseas y vuelve a licuar por unos segundos.','Sirve en un vaso y disfruta el batido.']}
+        />
+        
+        <CardBeverage
+            imagen={bebida05}
+            title='Infusión de jengibre y limón'
+            ingredients={['1 trozo de jengibre fresco, pelado y rallado','Jugo de 1 limón','2 cucharaditas de miel','1 taza de agua caliente']}
+            instructions={['En una tetera o recipiente, coloca el jengibre rallado y el jugo de limón.','Agrega el agua caliente y deja reposar durante 5 a 10 minutos.','Endulza con miel.','Cuela la infusión y sirve caliente.','Disfruta de esta bebida reconfortante y beneficiosa para la digestión.']}
+        />
+        
+        <CardBeverage
+            imagen={bebida06}
+            title='Té verde con menta y limón'
+            ingredients={['1 bolsitas de té verde','1 o 1/2 ramita de menta fresca','Jugo de medio limón','2 cucharaditas de miel','1 taza de agua caliente']}
+            instructions={['Coloca la bolsita de té verde y la ramita de menta en una tetera o recipiente.','Deja infusionar durante 3 a 5 minutos.','Retira la bolsita de té y la menta.','Agrega el jugo de limón y endulza con la miel','Revuelve bien y sirve caliente','Disfruta de este té refrescante y lleno de antioxidantes.']}
+        />
+
       </div>
     </div>
         
